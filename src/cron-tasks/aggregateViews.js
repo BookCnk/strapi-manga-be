098f9cb0e6aae1ce7cf59c96ec5375d025f8342c
sMercyX <titipong.githup@gmail.com>
2025-07-22 04:04:34 +0700

    [viewsManga] auto updates views every 30 mins

---
+++ src/cron-tasks/aggregateViews.js
@@ -0,0 +1,31 @@
+const cron = require('node-cron');
+
+module.exports = {
+  start(strapi) {
+    // Every 30 minutes
+    cron.schedule('*/30 * * * *', async () => {
+      try {
+        const mangas = await strapi.db.query("api::manga.manga").findMany({
+          populate: {
+            chapters: {
+              fields: ['views'],
+            },
+          },
+        });
+
+        for (const manga of mangas) {
+          const totalViews = manga.chapters.reduce((sum, chapter) => sum + (parseInt(chapter.views, 10) || 0), 0);
+
+          await strapi.db.query("api::manga.manga").update({
+            where: { id: manga.id },
+            data: {
+              views: totalViews,
+            },
+          });
+        }
+      } catch (error) {
+        console.error("❌ Cron Error:", error);
+      }
+    });
+  }
+};
